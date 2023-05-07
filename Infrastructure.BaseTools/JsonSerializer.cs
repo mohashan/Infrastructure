8a97f90935d98007fff8746c18cea4b14f2f59a2
mohashan <msh200x@gmail.com>
2023-05-07 21:47:54 +0330

    Add Serializer and deserializer extension Method

---
+++ Infrastructure.BaseTools/JsonSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.BaseTools
+{
+    public static class JsonSerializer
+    {
+        public static string JsonSerialize(this object obj,int maxDepth = 0,JsonSerializerDefaults defaultSerializer = JsonSerializerDefaults.Web)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions(defaultSerializer)
+            {
+                AllowTrailingCommas = true,
+                MaxDepth = maxDepth,
+                WriteIndented = true,
+            };
+
+            return System.Text.Json.JsonSerializer.Serialize(obj,options);
+        }
+
+        public static T JsonDeserialize<T>(this string jsonString)
+        {
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                throw new ArgumentException("Input string is empty");
+            }
+
+            return JsonSerializer.JsonDeserialize<T>(jsonString);
+        }
+    }
+}
