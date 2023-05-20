393dd76a2f393ffc46d58f48fcd09bfef36b5bc1
mohashan <msh200x@gmail.com>
2023-05-21 00:02:21 +0330

    Add Standard response to generic controller

--- Infrastructure.Messenger/Controllers/GenericController.cs
+++ Infrastructure.Messenger/Controllers/GenericController.cs
@@ -11,16 +11,16 @@ namespace Infrastructure.Messenger.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class GenericController<TEntity,TDto,TReadDto> : ControllerBase
-        where TEntity : BaseEntity<TEntity,TDto,TReadDto>
-        where TDto : BaseDto 
+    public class GenericController<TEntity, TDto, TReadDto> : ControllerBase
+        where TEntity : BaseEntity<TEntity, TDto, TReadDto>
+        where TDto : BaseDto
         where TReadDto : BaseReadDto
     {
         protected readonly MessengerDbContext ctx;
         private readonly AutoMapper.IConfigurationProvider configurationProvider;
         protected readonly AutoMapper.Mapper mapper;
 
-        public GenericController(MessengerDbContext context,AutoMapper.IConfigurationProvider configurationProvider)
+        public GenericController(MessengerDbContext context, AutoMapper.IConfigurationProvider configurationProvider)
         {
             this.ctx = context;
             this.configurationProvider = configurationProvider;
@@ -29,18 +29,18 @@ namespace Infrastructure.Messenger.Controllers
         [HttpGet]
         public async Task<ActionResult> Index(int count = 10, int pageNumber = 1)
         {
-            var result = ctx.Set<TEntity>().Skip(count * (pageNumber - 1)).Take(count).AsNoTracking().ProjectTo(typeof(TReadDto),configurationProvider);
+            var result = ctx.Set<TEntity>().Skip(count * (pageNumber - 1)).Take(count).AsNoTracking().ProjectTo(typeof(TReadDto), configurationProvider);
             return Ok(new StandardResponse<IQueryable>(true, null, result));
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult> Details(int id)
         {
-            var item = (await ctx.Set<TEntity>().FindAsync(id))??throw new ArgumentException($"There is no entry with this id(id:{id})");
+            var item = (await ctx.Set<TEntity>().FindAsync(id)) ?? throw new ArgumentException($"There is no entry with this id(id:{id})");
 
-            return Ok(new StandardResponse<TReadDto>(true,null, item.GetReadDto(mapper)));
+            return Ok(new StandardResponse<TReadDto>(true, null, item.GetReadDto(mapper)));
         }
-        
+
         [HttpPost]
         public virtual async Task<ActionResult> Create([FromBody] TDto dto)
         {
@@ -49,8 +49,8 @@ namespace Infrastructure.Messenger.Controllers
                 throw new ArgumentNullException(nameof(dto));
             }
 
-            var baseEnt = new Models.BaseEntity<TEntity,TDto,TReadDto>();
-            var entity = baseEnt.GetEntity(dto,mapper);
+            var baseEnt = new Models.BaseEntity<TEntity, TDto, TReadDto>();
+            var entity = baseEnt.GetEntity(dto, mapper);
 
             try
             {
@@ -61,11 +61,11 @@ namespace Infrastructure.Messenger.Controllers
             {
                 throw new ArgumentException("Create new Entity failed");
             }
-            
+
 
 
             return CreatedAtAction(nameof(Details),
-                new {  id = entity.Id },new StandardResponse<TReadDto>(true,null, entity.GetReadDto(mapper)));
+                new { id = entity.Id }, new StandardResponse<TReadDto>(true, null, entity.GetReadDto(mapper)));
         }
 
         [HttpPatch("{id:int}")]
@@ -76,44 +76,35 @@ namespace Infrastructure.Messenger.Controllers
                 throw new ArgumentNullException(nameof(patchDoc));
             }
 
-            var existingDto= (ctx.Set<TEntity>().Find(id)??throw new ArgumentException($"There is no entry with id : {id}")).GetDto(mapper);
+            var existingDto = (ctx.Set<TEntity>().Find(id) ?? throw new ArgumentException($"There is no entry with id : {id}")).GetDto(mapper);
 
 
             patchDoc.ApplyTo(existingDto);
 
-            var entity = new BaseEntity<TEntity,TDto,TReadDto>().GetEntity(existingDto, mapper);
+            var entity = new BaseEntity<TEntity, TDto, TReadDto>().GetEntity(existingDto, mapper);
 
             TryValidateModel(entity);
 
             ctx.Set<TEntity>().Update(entity);
 
 
-                ctx.SaveChanges();
+            ctx.SaveChanges();
 
 
-            return Ok(new StandardResponse<TEntity>(true,null, entity));
+            return Ok(new StandardResponse<TEntity>(true, null, entity));
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult Remove(int id)
         {
-            var entity = ctx.Set<TEntity>().Find(id)??throw new ArgumentException("$\"There is no entry with id : {id}");
+            var entity = ctx.Set<TEntity>().Find(id) ?? throw new ArgumentException($"There is no entry with id : {id}");
 
 
             entity.IsDeleted = true;
             entity.DeleteDate = DateTime.Now;
             ctx.Entry<TEntity>(entity).State = EntityState.Modified;
 
-            try
-            {
-
-                ctx.SaveChanges();
-            }
-            catch (Exception)
-            {
-                throw new Exception("Update an entity failed on save");
-            }
-
+            ctx.SaveChanges();
 
             return NoContent();
         }
@@ -123,30 +114,17 @@ namespace Infrastructure.Messenger.Controllers
         {
             if (dto == null)
             {
-                return BadRequest();
+                throw new ArgumentNullException(nameof(dto));
             }
 
-            var existingItem = ctx.Set<TEntity>().Find(id);
-
-            if (existingItem == null)
-            {
-                return NotFound();
-            }
+            var existingItem = ctx.Set<TEntity>().Find(id) ?? throw new ArgumentException($"There is no entry with id : {id}");
 
             existingItem.GetEntity(dto, mapper);
 
             ctx.Entry<TEntity>(existingItem).State = EntityState.Modified;
-            try
-            {
-                ctx.SaveChanges();
-            }
-            catch (Exception)
-            {
-                throw new Exception("Update a entity failed on save");
-            }
+            ctx.SaveChanges();
 
-
-            return Ok(existingItem.GetDto(mapper));
+            return Ok(new StandardResponse<TDto>(true, null, existingItem.GetDto(mapper)));
         }
     }
 }
